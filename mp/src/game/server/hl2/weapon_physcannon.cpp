5a9065576299c1da46048356b5ab1891411d5f03
The Apples Guy <33694155+Daisreich@users.noreply.github.com>
2024-02-24 08:24:18 -0500

    animations for physcannon

--- mp/src/game/server/hl2/weapon_physcannon.cpp
+++ mp/src/game/server/hl2/weapon_physcannon.cpp
@@ -1210,6 +1210,8 @@ BEGIN_SIMPLE_DATADESC( thrown_objects_t )
 	DEFINE_FIELD( hEntity,	FIELD_EHANDLE	),
 END_DATADESC()
 
+
+
 class CWeaponPhysCannon : public CWeaponDODBase
 {
 public:
@@ -1373,6 +1375,8 @@ protected:
 
 	float				m_flTimeNextObjectPurge;
 
+	DECLARE_ACTTABLE();
+
 protected:
 	// Because the physcannon is a leaf class, we can use
 	// static variables to store this information, and save some memory.
@@ -1434,6 +1438,40 @@ BEGIN_DATADESC( CWeaponPhysCannon )
 
 END_DATADESC()
 
+
+acttable_t CWeaponPhysCannon::m_acttable[] =
+{
+	{ ACT_DOD_STAND_AIM,					ACT_DOD_STAND_AIM_TOMMY,				false },
+	{ ACT_DOD_CROUCH_AIM,					ACT_DOD_CROUCH_AIM_TOMMY,				false },
+	{ ACT_DOD_CROUCHWALK_AIM,				ACT_DOD_CROUCHWALK_AIM_TOMMY,			false },
+	{ ACT_DOD_WALK_AIM,						ACT_DOD_WALK_AIM_TOMMY,					false },
+	{ ACT_DOD_RUN_AIM,						ACT_DOD_RUN_AIM_TOMMY,					false },
+	{ ACT_PRONE_IDLE,						ACT_DOD_PRONE_AIM_TOMMY,				false },
+	{ ACT_PRONE_FORWARD,					ACT_DOD_PRONEWALK_IDLE_TOMMY,			false },
+	{ ACT_DOD_STAND_IDLE,					ACT_DOD_STAND_IDLE_TOMMY,				false },
+	{ ACT_DOD_CROUCH_IDLE,					ACT_DOD_CROUCH_IDLE_TOMMY,				false },
+	{ ACT_DOD_CROUCHWALK_IDLE,				ACT_DOD_CROUCHWALK_IDLE_TOMMY,			false },
+	{ ACT_DOD_WALK_IDLE,					ACT_DOD_WALK_IDLE_TOMMY,				false },
+	{ ACT_DOD_RUN_IDLE,						ACT_DOD_RUN_IDLE_TOMMY,					false },
+	{ ACT_SPRINT,							ACT_DOD_SPRINT_IDLE_TOMMY,				false },
+
+	{ ACT_RANGE_ATTACK1,					ACT_DOD_PRIMARYATTACK_TOMMY,			false },
+	{ ACT_DOD_PRIMARYATTACK_CROUCH,			ACT_DOD_PRIMARYATTACK_TOMMY,			false },
+	{ ACT_DOD_PRIMARYATTACK_PRONE,			ACT_DOD_PRIMARYATTACK_PRONE_TOMMY,		false },
+	{ ACT_RANGE_ATTACK2,					ACT_DOD_SECONDARYATTACK_TOMMY,			false },
+	{ ACT_DOD_SECONDARYATTACK_CROUCH,		ACT_DOD_SECONDARYATTACK_CROUCH_TOMMY,	false },
+	{ ACT_DOD_SECONDARYATTACK_PRONE,		ACT_DOD_SECONDARYATTACK_PRONE_TOMMY,	false },
+
+	{ ACT_RELOAD,							ACT_DOD_RELOAD_TOMMY,					false },
+	{ ACT_DOD_RELOAD_CROUCH,				ACT_DOD_RELOAD_CROUCH_TOMMY,			false },
+	{ ACT_DOD_RELOAD_PRONE,					ACT_DOD_RELOAD_PRONE_TOMMY,				false },
+
+	// Hand Signals
+	{ ACT_DOD_HS_IDLE,						ACT_DOD_HS_IDLE_TOMMY,				false },
+	{ ACT_DOD_HS_CROUCH,					ACT_DOD_HS_CROUCH_TOMMY,			false },
+};
+
+IMPLEMENT_ACTTABLE(CWeaponPhysCannon);
 
 enum
 {

904256e9ec1573ecf02658e40186786fc97efeb7
The Apples Guy <33694155+Daisreich@users.noreply.github.com>
2024-02-24 10:31:42 -0500

    viewmodel lag

--- mp/src/game/shared/dod/dod_viewmodel.cpp
+++ mp/src/game/shared/dod/dod_viewmodel.cpp
@@ -49,7 +49,7 @@ CDODViewModel::~CDODViewModel()
 ConVar cl_wpn_sway_interp( "cl_wpn_sway_interp", "0.1", FCVAR_CLIENTDLL );
 ConVar cl_wpn_sway_scale( "cl_wpn_sway_scale", "2.6", FCVAR_CLIENTDLL );
 #endif
-
+extern float g_fMaxViewModelLag;
 void CDODViewModel::CalcViewModelLag( Vector& origin, QAngle& angles, QAngle& original_angles )
 {
 #ifdef CLIENT_DLL
@@ -87,6 +87,58 @@ void CDODViewModel::CalcViewModelLag( Vector& origin, QAngle& angles, QAngle& or
 	// Now offset the origin using that.
 	vForwardDiff *= flSwayScale;
 	origin += forward*vForwardDiff.x + right*-vForwardDiff.y + up*vForwardDiff.z;
+
+	Vector vOriginalOrigin = origin;
+	QAngle vOriginalAngles = angles;
+
+	// Calculate our drift
+	Vector	forward;
+	AngleVectors(angles, &forward, NULL, NULL);
+
+	if (gpGlobals->frametime != 0.0f)
+	{
+		Vector vDifference;
+		VectorSubtract(forward, m_vecLastFacing, vDifference);
+
+		float flSpeed = 5.0f;
+
+		// If we start to lag too far behind, we'll increase the "catch up" speed.  Solves the problem with fast cl_yawspeed, m_yaw or joysticks
+		//  rotating quickly.  The old code would slam lastfacing with origin causing the viewmodel to pop to a new position
+		float flDiff = vDifference.Length();
+		if ((flDiff > g_fMaxViewModelLag) && (g_fMaxViewModelLag > 0.0f))
+		{
+			float flScale = flDiff / g_fMaxViewModelLag;
+			flSpeed *= flScale;
+		}
+
+		// FIXME:  Needs to be predictable?
+		VectorMA(m_vecLastFacing, flSpeed * gpGlobals->frametime, vDifference, m_vecLastFacing);
+		// Make sure it doesn't grow out of control!!!
+		VectorNormalize(m_vecLastFacing);
+		VectorMA(origin, 5.0f, vDifference * -1.0f, origin);
+
+		Assert(m_vecLastFacing.IsValid());
+	}
+
+	Vector right, up;
+	AngleVectors(original_angles, &forward, &right, &up);
+
+	float pitch = original_angles[PITCH];
+	if (pitch > 180.0f)
+		pitch -= 360.0f;
+	else if (pitch < -180.0f)
+		pitch += 360.0f;
+
+	if (g_fMaxViewModelLag == 0.0f)
+	{
+		origin = vOriginalOrigin;
+		angles = vOriginalAngles;
+	}
+
+	//FIXME: These are the old settings that caused too many exposed polys on some models
+	VectorMA(origin, -pitch * 0.035f, forward, origin);
+	VectorMA(origin, -pitch * 0.03f, right, origin);
+	VectorMA(origin, -pitch * 0.02f, up, origin);
 #endif
 }
 
